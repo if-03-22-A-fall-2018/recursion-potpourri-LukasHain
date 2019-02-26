d0e987f92daef756060cc43489b15f13a155ddcc
LukasHain <lukashain9@gmail.com>
2019-02-26 11:33:57 +0100

    compiling this family friendly piece of c code

---
+++ recfuncs.cpp
@@ -0,0 +1,26 @@
+#include "recfuncs.h"
+
+int calc_array_size(int n)
+{
+
+}
+int convert_to_binary(int n)
+{
+
+}
+int* create_array(int size)
+{
+
+}
+int calc_factorial(int n)
+{
+
+}
+void calc_sums(int* arr, int size, int* out_arr, int* out_arr_bin)
+{
+
+}
+struct CalculationResults perform_calculations(int* arr, int n)
+{
+
+}
--- recfuncs.h
+++ recfuncs.h
@@ -1,6 +1,19 @@
 #ifndef ___RECURSIVE_POTPOURRI
 #define ___RECURSIVE_POTPOURRI
 
+struct CalculationResults {
+  int 	no_of_nums;
+  int 	no_of_sums;
+  int * 	sums;
+  int * 	binary_sums;
+  int * 	num_factorials;
+};
 
+int calc_array_size(int n);
+int convert_to_binary(int n);
+int* create_array(int size);
+int calc_factorial(int n);
+void calc_sums(int* arr, int size, int* out_arr, int* out_arr_bin);
+struct CalculationResults perform_calculations(int* arr, int n);
 
 #endif
